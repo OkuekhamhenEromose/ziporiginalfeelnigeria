fb82043363ac091587016ef073de1c0faa215ceb
Charle Eromose <okuekhamhene@gmail.com>
2025-10-28 07:21:25 +0100

    updated owambe

--- src/components/Owambe/OwambeUnplugged.tsx
+++ src/components/Owambe/OwambeUnplugged.tsx
@@ -9,9 +9,9 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import owambe1 from "../../assets/img/nightlife6.jpg";
-import owambe2 from "../../assets/img/nightlife5.jpg";
-import owambe3 from "../../assets/img/nightlife1.jpg";
+import owambe1 from "../../assets/img/nightlife9.jpg";
+import owambe2 from "../../assets/img/nightlife7.jpg";
+import owambe3 from "../../assets/img/nightlife3.jpg";
 
 const owambeEvents = [
   {
