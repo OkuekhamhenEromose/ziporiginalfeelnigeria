e94a963df676c9424022c98ae99ecdb8f0e9575e
OkuekhamhenEromose <okuekhamhene@gmail.com>
2025-11-05 18:27:32 +0100

    updating vitecongig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import { fileURLToPath } from 'url'
+import { resolve, dirname } from 'path'
 
-// https://vite.dev/config/
+// Simulate __dirname in ES module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
 })
