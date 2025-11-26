c5748e087b3614ae7775e62ec9be42b16b682603
OkuekhamhenEromose <okuekhamhene@gmail.com>
2025-11-26 17:12:50 +0100

    update others

--- src/components/feelnigeriatourismexch/Stage1.tsx
+++ src/components/feelnigeriatourismexch/Stage1.tsx
@@ -516,8 +516,8 @@ React.useEffect(() => {
                           }}
                         >
                           <option value="">Select Gender</option>
-                          <option value="Male">Male</option>
-                          <option value="Female">Female</option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
                           <option value="other">Other</option>
                         </select>
                       </Field.Root>
